3b8c534af4428560cd68f87cd40eaeec335dbab1
C++ <cplusplus@rm11-08.trainctrs.trng>
2021-08-25 11:08:11 -0400

    Completed Ex 6-2

--- Day 3/Ch 6/Ex 6-2.cpp
+++ Day 3/Ch 6/Ex 6-2.cpp
@@ -1,3 +1,9 @@
+/*
+ * Raymond Rowland
+ * 25AUG21
+ * Create an account program so it accepts an email address from the user the checks to make sure it is valid.
+ * 
+ */
 #include <iostream>
 #include <string>
 
@@ -77,11 +83,63 @@ int main()
         }
     }
     cout << endl;
+    
+    // get the email
+    string email;
+    bool valid_email = false;
+    while (!valid_email) {
+
+        // set valid flag
+        valid_email = true;
+
+		// get password
+        cout << "Enter email: ";
+        getline(cin, email);
+
+        // make sure email has at least 7 characters
+        if (email.length() < 7) {
+            cout << "Email missing characters.\nPlease provide at least X@X.XX\n";
+            valid_email = false;
+        }
+
+        // make sure password includes a number
+        int atIndex = email.find_first_of("@");
+        if (atIndex == -1) {
+            cout << "Email must include a '@'.\n";
+            valid_email = false;
+        }
+        else
+        {
+            if(atIndex < 1)
+            {
+                cout << "Email have character before the '@'.\n";
+                valid_email = false;
+            }
+        }
+
+        int dotIndex = email.find(".", atIndex);
+        if (dotIndex == -1) {
+            cout << "Email domain must include a '.'.\n";
+            valid_email = false;
+        }
+        
+        if((email.length - 1) - dotIndex < 2)
+        {
+            cout << "Email domain must be at least 2 characters.\n";
+            valid_email = false;
+        }
+
+        // display message if password is not valid
+        if (!valid_email) {
+            cout << "Please try again.\n";
+        }
+    }
+    cout << endl;
 
     // make sure first name uses initial cap
     char letter = first_name[0];
     first_name[0] = toupper(letter);
-    for (int i = 1; i < first_name.length(); ++i) {
+    for (uint i = 1; i < first_name.length(); ++i) {
         letter = first_name[i];
         first_name[i] = tolower(letter);
     }
