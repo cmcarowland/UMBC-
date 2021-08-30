24d19f886b592f0c597b629ceb5a6f14862d1c66
C++ <cplusplus@rm11-08.trainctrs.trng>
2021-08-30 15:39:58 -0400

    Completed Ex 11-2

--- Day 6/Ch11/Ex11-2.cpp
+++ Day 6/Ch11/Ex11-2.cpp
@@ -1,13 +1,31 @@
+/*
+ * Raymond Rowland
+ * 30AUG21
+ * Ex 11-2
+ * Modify the word counter program so it uses algorithms with the pairs of values in the map that contains the words and count of each word
+ */
 #include <iostream>
 #include <fstream>
+/*
+ * Raymond Rowland
+ * 30AUG21
+ * Ex 11-2
+ * Create prototype for a function named display that accepts a pair of string/int values and returns void
+ *  - To be use in for_each to display "key=value"
+ * Create prototype for a function named over_1 that accepts a pair of string/int values and returns bool
+ *  - If count is > 1 return true
+ */
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
 vector<string> load_words(string filename);
 map<string, int> get_word_count(const vector<string>& words);
+void display(pair<string, int> p);
+bool over_1(pair<string, int> p);
 
 int main() {
 	cout << "The Word Counter program\n\n";
@@ -30,10 +48,21 @@ int main() {
 	cout << endl << endl;
 
 	cout << "COUNT PER WORD: ";
-	for (auto pair : word_count) {
-		cout << pair.first << '=' << pair.second << ' ';
-	}
+	for_each(word_count.begin(), word_count.end(), display);
+	
 	cout << endl << endl;
+	
+	cout << count_if(word_count.begin(), word_count.end(), over_1) << " DUPLICATE WORDS" << endl;
+}
+
+void display(pair<string, int> p)
+{
+	cout << p.first << "=" << p.second << " ";
+}
+
+bool over_1(pair<string, int> p)
+{
+	return p.second > 1;
 }
 
 vector<string> load_words(string filename) {
