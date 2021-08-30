f50dc60ac9e2d6cd5f1b0aeb962258069cbbc82c
C++ <cplusplus@rm11-08.trainctrs.trng>
2021-08-30 14:59:09 -0400

    Completed Ex 10-3

--- Day 6/Ch10/Ex10-3.cpp
+++ Day 6/Ch10/Ex10-3.cpp
@@ -1,13 +1,35 @@
+/*
+ * Raymond Rowland
+ * 30AUG21
+ * Modify word counter program so it stores all the words in the text file in a multiset and all the unique words in a set.
+ * - Modify display_and_load_words to use multiset
+ * - Modify Main to display the count of words in the multiset 
+ * - Add function get_unique_words that accepts a multiset and returns a set
+ * - Modify main to user get_unique_words function
+ * - Modify the main function to display the count of unique words and unique words
+ * - Modify the main function to display all the unique words and the count that appears in the document.
+ */
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
-vector<string> display_and_load_words(string filename);
+multiset<string> display_and_load_words(string filename);
 map<string, int> get_word_count(const vector<string>& words);
+
+set<string> get_unique_words(multiset<string> &words)
+{
+	set<string> s;
+	for(auto w: words)
+		s.insert(w);
+	
+	return s;
+}
 
 int main() {
     cout << "The Word Counter program\n\n";
@@ -15,31 +37,30 @@ int main() {
     string filename = "dickens.txt";
     auto words = display_and_load_words(filename);
 
-    cout << words.size() << " WORDS: ";
-    for (string word : words) {
-        cout << word << ' ';
-    }
+    cout << "WORD COUNT: " << words.size();
     cout << endl << endl;
 
-    auto word_count = get_word_count(words);
+	auto unique = get_unique_words(words);
 
-    cout << word_count.size() << " UNIQUE WORDS: ";
-    for (auto pair : word_count) {
-        cout << pair.first << ' ';
+    cout << unique.size() << " UNIQUE WORDS: ";
+    for (auto pair : unique) {
+        cout << pair << ' ';
     }
     cout << endl << endl;
 
     cout << "COUNT PER WORD: ";
-    for (auto pair : word_count) {
-        cout << pair.first << '=' << pair.second << ' ';
+    for (auto pair : unique)
+    {
+        cout << pair << '=' << count(words.begin(), words.end(), pair) << ' ';
     }
     cout << endl << endl;
 }
 
-vector<string> display_and_load_words(string filename) {
-    vector<string> words;
+multiset<string> display_and_load_words(string filename) {
+    multiset<string> words;
     ifstream infile(filename);
-
+	cout << "FILE TEXT: ";
+	
     if (infile) {
         string word;
         while (infile >> word) {
@@ -57,14 +78,15 @@ vector<string> display_and_load_words(string filename) {
                 }
             }
 			cout << new_word << ' ';
-            words.push_back(new_word);      // add word 
+            words.insert(new_word);      // add word 
         }
         infile.close();
     }
+    cout << endl << endl;
     return words;
 }
 
-map<string, int> get_word_count(const vector<string>& words) {
+map<string, int> get_word_count(const multiset<string>& words) {
     map<string, int> word_count{};
 
     for (string word : words) {
