5134b66119de3b31c9455e1c533e4c021fb46cca
C++ <cplusplus@rm11-08.trainctrs.trng>
2021-08-30 14:43:37 -0400

    Completed Ex 10-2

--- Day 6/Ch10/Ex10-2.cpp
+++ Day 6/Ch10/Ex10-2.cpp
@@ -1,6 +1,15 @@
+/*
+ * Raymond Rowland
+ * 30AUG21
+ * - Add code to diaplay_codes function so it displays the codes properly
+ * - Add to displat_country so it gets an iterator for the element whose key is equal to the code
+ *   - Display approperiate code if not found
+ * - Add code to main that calls display codes when the program starts and any time the user chooses to cointinue
+ */
 #include <iostream>
 #include <map>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,6 +24,7 @@ char get_choice() {
 void display_codes(const map<string, string>& countries) {
 	cout << "Country codes: ";
 	// display codes
+	for_each(countries.begin(), countries.end(), [&](pair<string, string> country){cout << country.first << " ";}); 
 
 	cout << endl << endl;
 }
@@ -29,9 +39,18 @@ void display_country(const map<string, string>& countries) {
 	}
 
 	// get iterator for map element with code
+	auto country = countries.find(upper_code);
 
 	// if the code isn't found, display a message
+	if(country == countries.end())
+	{
+		cout << "Country Code Not Found\n\n";
+	}
 	// if the code is found, display the country name
+	else
+	{
+		cout << "You Selected " << country->second << "!\n\n";
+	}
 }
 
 int main()
@@ -40,8 +59,14 @@ int main()
 
 	cout << "The Country Codes Program\n\n";
 
-	// display the country codes
+	do
+	{
+		// display the country codes
+		display_codes(countries);
 
-	// while the user indicates they want to continue, display a country
+		// while the user indicates they want to continue, display a country
+		display_country(countries);
+		
+	}while(get_choice() == 'y');
 }
 
