24f62f465ab026735462af6d033e7e65dd210655
C++ <cplusplus@rm11-08.trainctrs.trng>
2021-08-31 15:48:35 -0400

    Completed Ex 15-2
    feat: Added used book class.
    fix: Added proper overriding tags to Movie and book.
    refactor: Changed main code to use new types and test overriding.

---
+++ Day 8/Ex15-2/UsedBook.h
@@ -0,0 +1,19 @@
+#ifndef MURACH_USEDBOOK_H
+#define MURACH_USEDBOOK_H
+
+#include <string>
+#include "Book.h"
+
+class UsedBook : public Book {
+
+public:
+    UsedBook(std::string name = "", double price = 0.0, 
+         int disc_pct = 0, std::string author_param = "") :
+         Book(name, price, disc_pct, author_param) {
+    }
+
+    std::string get_description() const override {
+        return name + " by " + author + " (USED)";
+    }
+};
+#endif // MURACH_BOOK_H
