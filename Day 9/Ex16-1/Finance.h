79da233049bca72cd9ffa1fc2b6083ef6e4f691d
C++ <cplusplus@rm11-08.trainctrs.trng>
2021-09-01 08:09:41 -0400

    Completed Ex 16-1
    feat: Completed Ex 16-1
    feat: Created new class console to handle user input.
    feat: Created new class Finance to handle calculations of interest over time.
    refactor: Main() to use the new console and finance classes
    chore: Added files for Ex 16-2

---
+++ Day 9/Ex16-1/Finance.h
@@ -0,0 +1,13 @@
+#ifndef MURACH_FINANCE_H
+#define MURACH_FINANCE_H
+
+class Finance
+{
+public:
+	// declare program-specific function
+	static double calculate_future_value(double monthly_investment,
+								  double yearly_interest_rate, int years);
+    
+};
+
+#endif
